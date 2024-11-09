6025bfd646afc6862fa01101890819634eb36308
IT24610821 <it24610821@my.sliit.lk>
2024-11-09 10:49:28 +0530

    Add files via upload

---
+++ Q4/IT24610821LAB3Q4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class IT24610821LAB3Q4{
+	public static void main(String [] args){
+	Scanner Input = new Scanner(System.in);
+	System.out.print("Enter a five-digit number: ");
+	int number = Input.nextInt();
+	if (number <10000 || number>99999){
+		System.out.println("Enter Correct Number");
+	}else{
+	int digit1 = number/10000;
+	int digit2 = (number % 10000)/1000;
+	int digit3 = (number % 1000)/100;
+	int digit4 = (number % 100)/10;
+	int digit5 = number % 10;
+	System.out.print(digit1+" "+digit2+" "+digit3+" "+digit4+" "+digit5);
+	}
+	}
+}
